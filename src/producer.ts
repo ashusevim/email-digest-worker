65ffe7092b92f83229c88985614c52d40ebca12b
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-12-20 22:26:10 +0530

    added job adding function

--- src/producer.ts
+++ src/producer.ts
@@ -1,1 +1,26 @@
-import queue from "bullmq";
+import { Queue } from "bullmq";
+
+interface userData {
+    userId: number;
+    email: string;
+}
+
+// connect to queue named
+const emailQueue = new Queue("email-queue", {
+    connection: {
+        host: "127.0.0.1",
+        port: 6379,
+    },
+});
+
+async function addJob() {
+    const job = await emailQueue.add("send-digest", {
+        userId: 1,
+        email: "user1@gmail.com",
+    });
+
+    console.log(`Job added with Id: ${job.userId}`);
+    process.exit();
+}
+
+addJob();
